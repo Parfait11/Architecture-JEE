e8ea7ccc8ebb3c2306c776aefecd10fd40fcdccb
Parfait11 <106759791+Parfait11@users.noreply.github.com>
2026-07-08 20:08:59 +0100

    added corsconfig and restController in web files

--- ebanking_backend/src/main/java/enset/dimba/ebanking_backend/web/CustomerRestController.java
+++ ebanking_backend/src/main/java/enset/dimba/ebanking_backend/web/CustomerRestController.java
@@ -3,7 +3,8 @@ package enset.dimba.ebanking_backend.web;
 import enset.dimba.ebanking_backend.dtos.CustomerDTO;
 import enset.dimba.ebanking_backend.exceptions.CustomerNotFoundException;
 import enset.dimba.ebanking_backend.services.BankAccountService;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,41 +12,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin("*")
 public class CustomerRestController {
+
     private BankAccountService bankAccountService;
 
-    public CustomerRestController() {
-    }
-
-    @GetMapping({"/customers"})
+    @GetMapping("/customers")
     public List<CustomerDTO> customers() {
-        return this.bankAccountService.listCustomers();
+        return bankAccountService.listCustomers();
     }
 
-    @GetMapping({"/customers/search"})
-    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword) {
-        return this.bankAccountService.searchCustomers("%" + keyword + "%");
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        return bankAccountService.searchCustomers("%" + keyword + "%");
     }
 
-    @GetMapping({"/customers/{id}"})
+    @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
-        return this.bankAccountService.getCustomer(customerId);
+        return bankAccountService.getCustomer(customerId);
     }
 
-    @PostMapping({"/customers"})
+    @PostMapping("/customers")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
-        return this.bankAccountService.saveCustomer(customerDTO);
+        return bankAccountService.saveCustomer(customerDTO);
     }
 
-    @PutMapping({"/customers/{customerId}"})
+    @PutMapping("/customers/{customerId}")
     public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO) {
         customerDTO.setId(customerId);
-        return this.bankAccountService.updateCustomer(customerDTO);
+        return bankAccountService.updateCustomer(customerDTO);
     }
 
-    @DeleteMapping({"/customers/{id}"})
+    @DeleteMapping("/customers/{id}")
     public void deleteCustomer(@PathVariable Long id) {
-        this.bankAccountService.deleteCustomer(id);
+        bankAccountService.deleteCustomer(id);
     }
 }
